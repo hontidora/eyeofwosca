510b6abae56b322454ebd077b97c4066fbdb333d
dorah <hontidora@gmail.com>
2019-02-13 19:58:07 +0100

    scroll

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -1,9 +1,9 @@
 (function () {
-    var renderer = new PIXI.autoDetectRenderer('1900', '1730', {transparent: true});
-    var stage = new PIXI.Container();
-    var slidesContainer = new PIXI.Container();
-    var displacementSprite = new PIXI.Sprite.fromImage('public/dist/img/clouds.jpg');
-    var displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+    let renderer = new PIXI.autoDetectRenderer('1920', '1080', {transparent: true});
+    let stage = new PIXI.Container();
+    let slidesContainer = new PIXI.Container();
+    let displacementSprite = new PIXI.Sprite.fromImage('public/dist/img/clouds.jpg');
+    let displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
 
     this.initPixi = function () {
         document.body.appendChild(renderer.view);
@@ -16,7 +16,7 @@
         renderer.view.style.left = '50%';
         renderer.view.style.webkitTransform = 'translate( -50%, -50% )';
         renderer.view.style.transform = 'translate( -50%, -50% )';
-        renderer.resize(window.innerWidth, 810)
+        renderer.resize(window.innerWidth, 810);
 
         displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
 
@@ -30,24 +30,23 @@
     };
 
     this.loadPixiSprite = function (sprites) {
-        var texture = new PIXI.Texture.fromImage(document.querySelector('.header__background').getAttribute('src'));
-        var image = new PIXI.Sprite(texture);
+        let texture = new PIXI.Texture.fromImage(document.querySelector('.header__background').getAttribute('src'));
+        let image = new PIXI.Sprite(texture);
 
         slidesContainer.addChild(image);
     };
 
-    var ticker = new PIXI.ticker.Ticker();
+    let ticker = new PIXI.ticker.Ticker();
     ticker.autoStart = true;
     ticker.add(function (delta) {
-
         displacementSprite.x += 0.3 * delta;
         displacementSprite.y += 0.3;
 
         renderer.render(stage);
     });
 
-    var mouseX = window.innerWidth / 2;
-    var mouseY = 400;
+    let mouseX = window.innerWidth / 2;
+    let mouseY = 100;
     TweenLite.to(displacementFilter.scale, 1, {
         x: "+=" + Math.sin(mouseX) * 1200 + "",
         y: "+=" + Math.cos(mouseY) * 200 + ""
@@ -57,23 +56,40 @@
     this.loadPixiSprite();
 })();
 
+(function () {
+    let jsSocialShares = document.querySelectorAll(".contact__item--share a");
+    if (jsSocialShares) {
+        [].forEach.call(jsSocialShares, function (anchor) {
+            anchor.addEventListener("click", function (e) {
+                e.preventDefault();
 
-var jsSocialShares = document.querySelectorAll(".contact__item--share a");
-if (jsSocialShares) {
-    [].forEach.call(jsSocialShares, function(anchor) {
-        anchor.addEventListener("click", function(e) {
-            e.preventDefault();
-
-            windowPopup(this.href, 500, 400);
+                windowPopup(this.href, 500, 400);
+            });
         });
-    });
-}
+    }
 
-function windowPopup(url, width, height) {
-    var left = (screen.width / 2) - (width / 2),
-        top = (screen.height / 2) - (height / 2);
+    function windowPopup(url, width, height) {
+        let left = (screen.width / 2) - (width / 2),
+            top = (screen.height / 2) - (height / 2);
 
-    window.open(
-        url, "", "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=" + width + ",height=" + height + ",top=" + top + ",left=" + left
-    );
-}
+        window.open(
+            url, "", "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=" + width + ",height=" + height + ",top=" + top + ",left=" + left
+        );
+    }
+})();
+
+(function () {
+    let anchorlinks = document.querySelectorAll('a[href^="#"]')
+
+    for (let item of anchorlinks) {
+        item.addEventListener('click', (e) => {
+            let hashval = item.getAttribute('href');
+            let target = document.querySelector(hashval);
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+            e.preventDefault();
+        })
+    }
+})();
